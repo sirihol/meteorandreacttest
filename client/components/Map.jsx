f427bd7ee2507c37e8750247942cb6ae0bdc6c91
Siri Holtnæs <siriholtnaes@Siris-MacBook-Air.local>
2016-01-18 19:25:10 +0100

    Added modal and icon hints

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,20 +1,85 @@
+Meteor.startup(function() {
+  Mapbox.load();
+});
+
+Tracker.autorun(function () {
+  if (Mapbox.loaded()) {
+    L.mapbox.accessToken = Meteor.settings.public.accessToken;
+    var map = L.mapbox.map("map", Meteor.settings.public.mapId);
+ }
+});
+
 Map = React.createClass({
-	render() {
+  getInitialState() {
+    return {
+      showModal: false
+    }
+  },
+  showModal(modalType) {
+    this.setState({
+      showModal: modalType
+    })
+  },
+  hideModal(e) {
+    this.setState({
+      showModal: false
+    })
+  },
+
+  render() {
 		return (
+      <div>
+  			<Sidenav showModal={this.showModal}/>
 			<div className="content-wrapper">
-				<Sidenav />
-				<Modal />
+        <Modal
+          showModal={this.state.showModal}
+          hideModal={this.hideModal}/>
+				<div id="map" className="mapbox"></div>
 			</div>
+    </div>
 		)
 	}
 })
 
 Sidenav = React.createClass({
-	render() {
+  getInitialState() {
+    return {
+      tooltipDescription: "",
+      showTooltip: false,
+      tooltipX: "50px",
+      tooltipY: "0px"
+    }
+  },
+  showTooltip(e) {
+    this.setState({
+      showTooltip: true,
+      tooltipY: e.nativeEvent.target.offsetTop + (e.nativeEvent.target.offsetHeight / 2) + "px"
+    })
+  },
+  setTooltipDescription(item) {
+    this.setState({
+      tooltipDescription: item.description
+    })
+  },
+  hideTooltip(e) {
+    this.setState({
+      showTooltip: false
+    })
+  },
+  render() {
 		return (
 			<nav className="sidenav">
+      <SidenamTooltip
+        showTooltip={this.state.showTooltip}
+        tooltipDescription={this.state.tooltipDescription}
+        tooltipX={this.state.tooltipX}
+        tooltipY={this.state.tooltipY}/>
 			<ul className="sidenav-list">
-					<SidenavIcons />
+					<SidenavIcons
+            showModal={this.props.showModal}
+            setTooltipDescription={this.setTooltipDescription}
+            showTooltip={this.showTooltip}
+            hideTooltip={this.hideTooltip} />
 				</ul>
 			</nav>
 		)
@@ -30,13 +95,16 @@ SidenavIcons = React.createClass({
 	render(){
 		let iconList = [
 			{name: "fa fa-plus", description: "Nytt litterært sted" },
-			{name: "fa fa-location-arrow", description: "Ny litterær løype" },
-			{name: "fa fa-user", description: "Min profil" }
+ 			{name: "fa fa-user", description: "Min profil" }
 		]
 
 		let list = iconList.map((item) => {
 			return (
 				<li key={item.name}
+          onClick={this.props.showModal.bind(null, item.description)}
+          onMouseEnter={this.props.setTooltipDescription.bind(null, item)}
+          onMouseOver={this.props.showTooltip}
+          onMouseOut={this.props.hideTooltip}
 					className="sidenav-list-item">
 					<i className={item.name}></i>
 				</li>
@@ -48,4 +116,26 @@ SidenavIcons = React.createClass({
       </div>
     )
 	}
+})
+
+SidenamTooltip = React.createClass({
+  render(){
+    tooltipStyle = {
+      top: this.props.tooltipY,
+      left: this.props.tooltipX
+    }
+    if (this.props.showTooltip) {
+      tooltipStyle.opacity = "1";
+      tooltipStyle.visibility = "visible";
+    } else {
+      tooltipStyle.opacity = "0";
+      tooltipStyle.visibility = "hidden";
+    }
+    return(
+      <div className="sidenav-tooltip" style={tooltipStyle}>
+        <p>{this.props.tooltipDescription}</p>
+        <div className="tail"></div>
+      </div>
+    )
+  }
 })
--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -1,9 +1,25 @@
 Modal = React.createClass({
   render() {
+    let modalStyle = {
+      visibility: "hidden",
+      opacity: "0"
+    }
+    if (!!this.props.showModal) {
+      modalStyle.visibility =  "visible"
+      modalStyle.opacity = "1"
+    }
+
     return (
-        <div className="modal-active-darken">
+        <div className="modal-active-darken" style={modalStyle}>
           <div className="modal-container">
-            <AddLiteraryPlace />
+            {(() => {
+              switch (this.props.showModal) {
+                case "Nytt litterært sted": return <AddLiteraryPlace
+                  hideModal={this.props.hideModal}/>;
+                case "Min profil": return <h1>Logg inn</h1>;
+                default: return false;
+                }
+              })()}
           </div>
         </div>
     )
@@ -14,10 +30,10 @@ AddLiteraryPlace = React.createClass ({
   render() {
     return (
       <div>
-
         <div className="literary-form">
           <h1>Legg til litterært sted</h1>
-          <div className="close-modal-x">&times;</div>
+          <div className="close-modal-x"
+            onClick={this.props.hideModal}>&times;</div>
           <input type="text" placeholder="Forfatter" />
           <input type="text" placeholder="Litterært verk" />
           <input type="text" placeholder="År" />
@@ -26,9 +42,10 @@ AddLiteraryPlace = React.createClass ({
         </div>
 
         <div className="submit-modal-buttons">
-            <button className="button">Lagre</button>
+            <button
+              onClick={this.props.hideModal}
+              className="button">Lagre</button>
         </div>
-
       </div>
     )
   }
