d35976f0abd445f649dad44a6b3ffe655aa8a42f
Vikas Gupta <gvikas91@gmail.com>
2016-03-22 15:19:14 +0100

    Added function: If user is not logged in, redirect to login page

---
+++ client/components/accounts/accounts.js
@@ -0,0 +1,2 @@
+Template['override-atPwdFormBtn'].replaces('atPwdFormBtn');
+Template['override-atPwdForm'].replaces('atPwdForm');
--- client/router.jsx
+++ client/router.jsx
@@ -1,21 +1,26 @@
 const {Router, Route, Rederict} = ReactRouter;
 
-const history = ReactRouter.history.useQueries(ReactRouter.history.createHistory)()
+const history = ReactRouter.history.useQueries(ReactRouter.history.createHistory)();
+
+function requireAuth(nextState, replaceState) {
+  if (!Meteor.user())
+    replaceState({ nextPathname: nextState.location.pathname }, '/login')
+}
 
 if (Meteor.isClient) {
 	Meteor.startup(function() {
 	  	let AppRoutes = (
 	    <Router history= {history}>
 	      <Route component={App}>
-					<Router component={LiteraryTrail} path="literaryTrail/:id" />
-					<Router component={LiteraryTrails} path="literaryTrails" />
-					<Router component={Map} path="/" />
-					<Router component={Profile} path="profile" />
-					<Router component={LiteraryTrailMap} path="literaryTrail/map/:id" />
+					<Router component={LiteraryTrail} path="literaryTrail/:id" onEnter={requireAuth}/>
+					<Router component={LiteraryTrails} path="literaryTrails" onEnter={requireAuth}/>
+					<Router component={Map} path="/" onEnter={requireAuth} />
+	        		<Router component={AccountsUIWrapper} path="login" />
+					<Router component={Profile} path="profile" onEnter={requireAuth} />
+					<Router component={LiteraryTrailMap} path="literaryTrail/map/:id" onEnter={requireAuth} />
 	      </Route>
 	    </Router>
 	  )
 	  ReactDOM.render(AppRoutes, document.getElementById("render-target"));
 	})
 }
-	        //<Router component={AccountsUIWrapper} path="/" />
